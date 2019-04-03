c91a3819551c418ae828aaf44eaa5a2a09782c72
Sharma <ravikant.sharma@cybergindia.local>
2019-04-03 17:20:02 +0530

    youtube code added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-//import {Component} from 'react';
-import './App.css';
+import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-function App() {
+function NavBar() {
   return (
     <div className="NavBar">
       <div className="row">
@@ -18,7 +17,7 @@ function App() {
         </div>
         <div className="col-md-4 col-sm-4">
           <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search Youtube" id="myInput" onkeyup="searchbox()" />
+            <input type="text" className="form-control" placeholder="Search Youtube" id="SearchInput"  />
             <div className="input-group-append">
               <span className="input-group-text"><button style={{ padding: '0px' }}><i style={{ fontSize: '16px' }} className="fas fa-search ml-1 mt-1"  ></i></button></span>
             </div>
@@ -30,7 +29,7 @@ function App() {
             <button type="button" id="Apps"><i className="fab fa-youtube-square mr-2"></i></button>
             <button type="button" id="Share"><i className="fas fa-share-square mr-2"></i></button>
             <button type="button" id="Notification"><i className="fas fa-bell mr-2"></i></button>
-            <button type="button" id="Profile"><i class="fas fa-users mr-2"></i></button>
+            <button type="button" id="Profile"><i className="fas fa-users mr-2"></i></button>
           </div>
         </div>
 
@@ -39,4 +38,23 @@ function App() {
   );
 }
 
-export default App;
+export default NavBar;
+
+
+// var VideoTitleListTemp = ["How to make pastasdhf sjdfbjh agsdf asufbsbf shbf ashfb asdf shbfj asf adsjf j", "Football WC ", "Tom and jerry Show", "What if the earth loses oxygen", "The Cold War"];
+// //This Function Provide sthe search Functionality and shows only Relevant results
+// function SearchBox(event) {
+//   console.log(event.target.value);
+//   var abcd=event.target.value;
+//   //let resultList = document.querySelector(".result");
+//   //resultList.innerHTML = "";
+
+//   VideoTitleListTemp.map(function(algo){
+//     abcd.split(" ").map(function (word){
+//           if(algo.toLowerCase().indexOf(word.toLowerCase()) != -1){
+//             console.log(abcd);
+//               //resultList.innerHTML += `<li class="list-group-item">${algo}</li>`;
+//           }
+//       })
+//   })
+// }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './Navbar';
+import SideBar from './Sidebar'
+import VideoCategories from './VideoCategories';
 import './index.css';
-import App from './Navbar';
-//import App from './main';
 
-var VideoThumbnailList=[""];
-var VideoTitleList=["How to Make Pasta", "Football WC Highlights", "Tom and jerry Show", "What if the earth loses Oxygen", "The Cold War"];
-var ChannelNameList=["Kitchen King", "EsFootbal", "WB Kids", "What if","OverSimplified"];
-var ViewsCountList=[0,500,"100M", "2M", "6.4M"];
-var UploadTimeList=["6 hours ago","8 hours ago", "3 years ago", "24 hours ago", "1 day ago"];
 
-function DisplayVideos(){
-    return(
-        <div className="VideoCategory">
-        <App src={VideoThumbnailList[0]} VideoTitleList={VideoTitleList[0]} ChannelName={ChannelNameList[0]} ViewsCount={ViewsCountList[0]} UploadTime={UploadTimeList[0]}/>
-        <App src={VideoThumbnailList[1]} VideoTitleList={VideoTitleList[1]} ChannelName={ChannelNameList[1]} ViewsCount={ViewsCountList[1]} UploadTime={UploadTimeList[1]}/>
-        <App src={VideoThumbnailList[2]} VideoTitleList={VideoTitleList[2]} ChannelName={ChannelNameList[2]} ViewsCount={ViewsCountList[2]} UploadTime={UploadTimeList[2]}/>
-        <App src={VideoThumbnailList[3]} VideoTitleList={VideoTitleList[3]} ChannelName={ChannelNameList[3]} ViewsCount={ViewsCountList[3]} UploadTime={UploadTimeList[3]}/>
-        <App src={VideoThumbnailList[4]} VideoTitleList={VideoTitleList[4]} ChannelName={ChannelNameList[4]} ViewsCount={ViewsCountList[4]} UploadTime={UploadTimeList[4]}/>
+function HomePage() {
+    return (
+        <div>
+            <NavBar />
+            <div className="DisplayItems">
+                <div className="Sidebar"> <SideBar /></div>
+                <div className="VideoCategories">
+                    <div className="Videos"><VideoCategories /></div>
+                    <div className="Videos"><VideoCategories /></div>
+                    <div className="Videos"><VideoCategories /></div>
+                </div>
+            </div>
         </div>
-        );
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<HomePage />, document.getElementById('root'));
